e4fd0fcdabc284608aa9df9e68f65697aa971be6
varalika97 <varalikabishnoi@gmail.com>
2019-01-31 12:10:44 +0530

    Assignment on 31/01/19

---
+++ assignment1.ps1
@@ -0,0 +1,33 @@
+﻿function Divide()  #function for division
+{
+# Exception Handling
+try{
+
+
+$ans = $a/$b  #dividing the numbers
+ return $ans  #returning the value
+ 
+}
+catch [DivideByZeroException]
+{
+ Write-Host "Divide by zero exception"
+}
+}
+
+try
+{
+ Write-Host 'Enter a number'
+[int]$a = Read-Host             #user input
+Write-Host 'Enter a number'
+[int]$b = Read-Host             #user input
+
+$num =  divide $a $b           #calling the function and receving the value from the function
+Write-Host $num
+}
+
+#handlling other exception if occurs
+catch 
+{
+ Write-Host "Exception"
+ Write-Host $_.Exception.Message
+}
